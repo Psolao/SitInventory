bad1facca78ffc5319875c3ce4bfec9e85b93fe6
Psolao <psolao77@gmail.com>
2021-09-26 22:47:23 +0300

    Для теста скопированы данные

--- app/src/main/java/com/example/sitinventory/MainActivity.kt
+++ app/src/main/java/com/example/sitinventory/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.example.sitinventory
 
-import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
+import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.widget.Button
 import android.widget.EditText
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,70 +19,71 @@ class MainActivity : AppCompatActivity() {
     lateinit var barcodeEdit:EditText
     lateinit var resultEdit:EditText
     lateinit var addButton:Button
+    lateinit var dataFile: File
+
     val openDocLauncher = registerForActivityResult( ActivityResultContracts.OpenDocument()){
-        resultEdit.editableText.append(it.toString())
+        resultEdit.editableText.append(it.path)
+        copyFile(it, dataFile)
+        readFile()
     }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        requestAppPermissions()
         barcodeEdit = findViewById(R.id.barcode_text)
         resultEdit = findViewById(R.id.result_text)
         addButton = findViewById(R.id.add_button)
         initData()
+        addButton.setOnClickListener{selectFile()}
     }
 
     fun initData(){
-        val file = File(filesDir,"items.csv")
-        if (!file.exists()){
-           openDocLauncher.launch(arrayOf("*/*"))
+        dataFile = File(filesDir,"items.csv")
+        if (!dataFile.exists()){
+           selectFile()
+        }
+        else readFile()
+    }
+
+    fun selectFile(){
+        openDocLauncher.launch(arrayOf("*/*"))
+    }
+
+    fun readFile(){
+        var i = 0
+        dataFile.forEachLine {
+            if (i>5) return@forEachLine
+            i++
+            resultEdit.editableText.append(it)
         }
     }
 
-    fun testData(){
-        showDirInfo(Environment.getExternalStorageDirectory())
-    }
-
-    fun showDirInfo(file: File?){
-        if (file==null) resultEdit.editableText.appendLine("not found")
-        else {
-            resultEdit.editableText.appendLine(file.absolutePath)
-            file.list().forEach {
-                resultEdit.editableText.appendLine(it)
-            }
+    fun copyFile(uri:Uri, destFile: File) {
+        var source : InputStream? = null
+        var destination : OutputStream? = null
+        try {
+            source = contentResolver.openInputStream(uri)!!
+            destination = FileOutputStream(destFile)
+            copy(source,destination)
+        } finally {
+            source?.close()
+            destination?.close()
         }
     }
 
-
-    private fun requestAppPermissions() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            return
+    private val EOF = -1
+    private val DEFAULT_BUFFER_SIZE = 1024 * 4
+    private fun copy(input: InputStream, output: OutputStream): Long {
+        var count: Long = 0
+        var n: Int
+        val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
+        while (EOF !== input.read(buffer).also { n = it }) {
+            output.write(buffer, 0, n)
+            count += n.toLong()
         }
-        if (hasReadPermissions() && hasWritePermissions()) {
-            return
-        }
-        ActivityCompat.requestPermissions(
-            this, arrayOf(
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-            ), REQUEST_WRITE_STORAGE_CODE
-        ) // your request code
+        return count
     }
 
-    private fun hasReadPermissions(): Boolean {
-        return ContextCompat.checkSelfPermission(
-            baseContext,
-            Manifest.permission.READ_EXTERNAL_STORAGE
-        ) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun hasWritePermissions(): Boolean {
-        return ContextCompat.checkSelfPermission(
-            baseContext,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-        ) == PackageManager.PERMISSION_GRANTED
-    }
 }
 
